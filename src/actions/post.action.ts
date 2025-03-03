8a69ec54578a81cb0debb005e2b15d4a163aeae7
caesarc <46427941+caesarc6@users.noreply.github.com>
2025-03-03 00:29:24 -0500

    Initial commit

---
+++ src/actions/post.action.ts
@@ -0,0 +1,77 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { getDBUserId } from "./user.action";
+import { revalidatePath } from "next/cache";
+
+export async function createPost(content: string, image: string) {
+  try {
+    const userId = await getDBUserId();
+
+    if (!userId) return;
+
+    const post = await prisma.post.create({
+      data: {
+        content,
+        image,
+        authorId: userId,
+      },
+    });
+
+    revalidatePath("/");
+    return { success: true, post };
+  } catch (error) {
+    console.log("Failed to create post", error);
+    return { success: false, error: "Failed to create post" };
+  }
+}
+
+export async function getPosts() {
+  try {
+    const posts = await prisma.post.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        author: {
+          select: {
+            name: true,
+            image: true,
+            username: true,
+          },
+        },
+        comments: {
+          include: {
+            author: {
+              select: {
+                id: true,
+                username: true,
+                image: true,
+                name: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: "asc",
+          },
+        },
+        likes: {
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            likes: true,
+            comments: true,
+          },
+        },
+      },
+    });
+
+    return posts;
+  } catch (error) {
+    console.log("Error fetching posts", error);
+    throw new Error("Error fetching posts");
+  }
+}
